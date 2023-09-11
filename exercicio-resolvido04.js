e199e380fc95af5306679916a712f828b0214960
unknown <evandro.ucciolini@outlook.com>
2023-09-11 20:47:50 -0300

    Exercício 04 realizado!

---
+++ exercicio-resolvido04.js
@@ -0,0 +1,16 @@
+// Com string template
+
+let x = 10, y = 2
+
+let resultado = x + y;
+console.log(` A soma de ${x} e ${y} é ${resultado}`)
+
+resultado = x - y;
+console.log(` A subtração de ${x} menos ${y} é ${resultado}`)
+
+resultado = x * y;
+console.log(` A multiplicação de ${x} e ${y} é ${resultado}`)
+
+resultado = x / y;
+console.log(` A divisão de ${x} por ${y} é ${resultado}`)
+
