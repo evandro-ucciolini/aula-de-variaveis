681dfddd0ba493de0bb2589f6e4a400d239b2b69
unknown <evandro.ucciolini@outlook.com>
2023-09-11 20:42:30 -0300

    Exercício 03 realizado!

---
+++ exercicio-resolvido03.js
@@ -0,0 +1,18 @@
+let x = 10, y = 2
+
+let resultado = x + y;
+let mensagem = "A soma de " + x + " e " + y + " é " + resultado;
+console.log(mensagem)
+
+resultado = x - y;
+mensagem = "A subtração de " + x + " menos " + y + " é " + resultado;
+console.log(mensagem)
+
+resultado = x * y;
+mensagem = "A multiplicação de " + x + " e " + y + " é " + resultado;
+console.log(mensagem)
+
+resultado = x / y;
+mensagem = "A divisão de " + x + " por " + y + " é " + resultado;
+console.log(mensagem)
+
