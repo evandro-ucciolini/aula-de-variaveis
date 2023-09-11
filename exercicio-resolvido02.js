2ef1dbfe21d719197ae5431e3fd0e22b827086bc
unknown <evandro.ucciolini@outlook.com>
2023-09-11 19:54:20 -0300

    Exercício 02 realizado!

---
+++ exercicio-resolvido02.js
@@ -0,0 +1,23 @@
+// No ano de 2010
+let habitantes = 11_253_503
+
+console.log(habitantes)
+
+// No ano de 2023
+habitantes = 11_451_245
+
+console.log(habitantes)
+
+console.log("OU")
+
+// COM STRING TEMPLATE
+
+// No ano de 2010
+habitantes = 11_253_503
+
+console.log(`Em 2010, a cidade de São Paulo possuia ${habitantes} habitantes.`)
+
+// No ano de 2023
+habitantes = 11_451_245
+
+console.log(`Em 2023, a cidade de São Paulo possui ${habitantes} habitantes.`)
